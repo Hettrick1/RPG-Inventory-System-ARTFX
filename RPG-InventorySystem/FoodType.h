f8324b4c3221418b33e7e8e803bfcec743544b28
Hettrick1 <evann.jacquot@gmail.com>
2024-11-14 18:05:03 +0100

    [fix] error causing by type Type

--- RPG-InventorySystem/FoodType.h
+++ RPG-InventorySystem/FoodType.h
@@ -9,18 +9,18 @@ namespace FoodType {
 		Water		= 1 << 4
 	};
 
-	inline uint8_t operator + (uint8_t const& a, Type& b) {
-		return a | b;
+	inline uint8_t operator + (uint8_t& a, Type& b) {
+		return a | static_cast<uint8_t>(b);
 	}
 
-	inline uint8_t operator - (uint8_t const& a, Type& b) {
-		return a ^ b;
+	inline uint8_t operator - (uint8_t& a, Type& b) {
+		return a ^ static_cast<uint8_t>(b);
 	}
-	inline void operator += (uint8_t& a, Type const& b) {
-		a = a | b;
+	inline void operator += (uint8_t& a, Type b) {
+		a |= static_cast<uint8_t>(b);
 	}
 
-	inline void operator -= (uint8_t& a, Type const& b) {
-		a = a ^ b;
+	inline void operator -= (uint8_t& a, Type b) {
+		a ^= static_cast<uint8_t>(b);
 	}
 }
