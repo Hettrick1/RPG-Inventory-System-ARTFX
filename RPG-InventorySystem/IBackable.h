7b07ea470161ad66f28f0189454ff86a3f82875b
Hettrick1 <evann.jacquot@gmail.com>
2024-11-14 18:06:32 +0100

    [refactor] add pure virtual function + comments

--- RPG-InventorySystem/IBackable.h
+++ RPG-InventorySystem/IBackable.h
@@ -5,4 +5,7 @@
 class IBackable
 {
 public:
+	virtual bool canCraft() = 0; 
+	// the pure virtual function that is needed. 
+	// In my case it's not mendatory to have an interface to handle the crafting
 };
