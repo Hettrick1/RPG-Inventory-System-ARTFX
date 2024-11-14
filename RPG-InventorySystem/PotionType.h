7389185210d7e49a5492f3f82310263337b57998
Hettrick1 <evann.jacquot@gmail.com>
2024-11-14 18:05:10 +0100

    [fix] error causing by type Type

--- RPG-InventorySystem/PotionType.h
+++ RPG-InventorySystem/PotionType.h
@@ -7,18 +7,18 @@ namespace PotionType {
 		ManaRegen      = 1 << 2,
 		NightVision    = 1 << 3,
 	};
-	inline uint8_t operator + (uint8_t const& a, Type& b) {
-		return a | b;
+	inline uint8_t operator + (uint8_t& a, Type& b) {
+		return a | static_cast<uint8_t>(b);
 	}
 
-	inline uint8_t operator - (uint8_t const& a, Type& b) {
-		return a ^ b;
+	inline uint8_t operator - (uint8_t& a, Type& b) {
+		return a ^ static_cast<uint8_t>(b);
 	}
-	inline void operator += (uint8_t& a, Type const& b) {
-		a = a | b;
+	inline void operator += (uint8_t& a, Type b) {
+		a |= static_cast<uint8_t>(b);
 	}
 
-	inline void operator -= (uint8_t& a, Type const& b) {
-		a = a ^ b;
+	inline void operator -= (uint8_t& a, Type b) {
+		a ^= static_cast<uint8_t>(b);
 	}
 }
